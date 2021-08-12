78b1a1685fee0530ebf0dcfeb956c8fdfe3aa2e8
VyacheslavRus <78310890+VyacheslavRus@users.noreply.github.com>
2021-08-13 02:21:32 +0300

    ready rus task

--- src/components/ReportsExInSwitch/ReportExpense/NameCategories.js
+++ src/components/ReportsExInSwitch/ReportExpense/NameCategories.js
@@ -1,22 +1,23 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
-  { name: 'icon-products', category: 'Продукты' },
-  { name: 'icon-alcohol', category: 'Алкоголь' },
-  { name: 'icon-kite', category: 'Развлечения' },
+  { name: 'icon-products', category: 'продукты' },
+  { name: 'icon-alcohol', category: 'алкоголь' },
+  { name: 'icon-kite', category: 'развлечения' },
   {
     name: 'icon-hands-holding-heart',
-    category: 'Здоровье',
+    category: 'здоровье',
   },
-  { name: 'icon-car', category: 'Транспорт' },
-  { name: 'icon-couch', category: 'Всё для дома' },
-  { name: 'icon-tools', category: 'Техника' },
+  { name: 'icon-car', category: 'транспорт' },
+  { name: 'icon-couch', category: 'всё для дома' },
+  { name: 'icon-tools', category: 'техника' },
   {
     name: 'icon-invoice',
-    category: 'Коммуналка и связь',
+    category: 'коммуналка и связь',
   },
-  { name: 'icon-clay', category: 'Спорт и хобби' },
+  { name: 'icon-clay', category: 'спорт и хобби' },
   {
     name: 'icon-book',
-    category: 'Образование',
+    category: 'образование',
   },
-  { name: 'icon-ufo', category: 'Прочее' },
+  { name: 'icon-ufo', category: 'прочее' },
 ];
