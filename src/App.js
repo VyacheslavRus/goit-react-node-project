04eb31a2900e33d659ce22248bc8c730232d38cf
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-13 09:45:01 +0300

    добавил лоадер

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { useBreakpoint } from 'react-use-size';
 import Header from './components/Header/Header';
 import { routes, PublicRoute, PrivateRoute } from './routes';
 import * as authSelectors from './redux/selectors/authSelectors';
-import authOperations from './redux/operations/authOperations';
-import authActions from './redux/actions/authActions';
-import api from './services/kapusta-api' 
-import { autoInject } from 'async';
+// import authOperations from './redux/operations/authOperations';
+// import authActions from './redux/actions/authActions';
+import api from './services/kapusta-api';
+// import { autoInject } from 'async';
+import FallBack from './components/FallBackContainer/FallBackContainer';
 
 const AuthorizationView = lazy(() =>
   import(
@@ -38,35 +39,34 @@ export default function App() {
 
   useEffect(() => {
     console.log(token);
-    if (token) api.token.set(token)
-  }, [token])
+    if (token) api.token.set(token);
+  }, [token]);
 
-//   useEffect(() => {
-//     // При логинизации через Google в момент маунта App (componentDidMount) в адресной строке есть токен пользователя.
-//     // 1. Вытаскиваем токен пользователя из адресной строки и записываем его в переменную googleUserToken.
-//     const googleUserToken = new URLSearchParams(location.search).get(
-//       'accessToken',
-//     );
+  //   useEffect(() => {
+  //     // При логинизации через Google в момент маунта App (componentDidMount) в адресной строке есть токен пользователя.
+  //     // 1. Вытаскиваем токен пользователя из адресной строки и записываем его в переменную googleUserToken.
+  //     const googleUserToken = new URLSearchParams(location.search).get(
+  //       'accessToken',
+  //     );
 
-//     // 2. Если googleUserToken есть, то записываем его в наш Redux Store в свойсво token.
-//     googleUserToken && dispatch(authActions.setGoogleToken(googleUserToken));
+  //     // 2. Если googleUserToken есть, то записываем его в наш Redux Store в свойсво token.
+  //     googleUserToken && dispatch(authActions.setGoogleToken(googleUserToken));
 
-//     // 3. Если токен есть, а почты пользователя (user.email) нет, то делаем запрос на бекенд на получение текущего пользователя.
-//     // Проверка на токен и почту нужна, что б не делать лишние запросы на бекенд уже после логина пользователя.
-//     if (token && !userEmail) {
-//       dispatch(authOperations.getCurrentUser());
-//     }
-//   },[token])
-
+  //     // 3. Если токен есть, а почты пользователя (user.email) нет, то делаем запрос на бекенд на получение текущего пользователя.
+  //     // Проверка на токен и почту нужна, что б не делать лишние запросы на бекенд уже после логина пользователя.
+  //     if (token && !userEmail) {
+  //       dispatch(authOperations.getCurrentUser());
+  //     }
+  //   },[token])
 
   let x = location.pathname === '/authorization';
   let y = x ? 'main-bg-auth' : 'main-bg';
-      
+
   return (
     <>
       <Header />
       <div className={y}>
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<FallBack />}>
           <Switch>
             <PublicRoute path={routes.auth} restricted redirectTo={routes.home}>
               <AuthorizationView />
