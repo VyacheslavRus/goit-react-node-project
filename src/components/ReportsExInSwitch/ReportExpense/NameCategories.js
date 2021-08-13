068064ef340c54e2ffe8994d4bdca6af4a5122bc
Georgiy Tonkiy <georgiy.tonkiy@gmail.com>
2021-08-13 12:46:03 +0300

    Подключил чарт и почистил код

--- src/components/ReportsExInSwitch/ReportExpense/NameCategories.js
+++ src/components/ReportsExInSwitch/ReportExpense/NameCategories.js
@@ -2,22 +2,13 @@
 export default [
   { name: 'icon-products', category: 'продукты' },
   { name: 'icon-alcohol', category: 'алкоголь' },
-  { name: 'icon-kite', category: 'развлечения' },
-  {
-    name: 'icon-hands-holding-heart',
-    category: 'здоровье',
-  },
+  { name: 'icon-kite', category: 'развлечение' },
+  { name: 'icon-hands-holding-heart', category: 'здоровье' },
   { name: 'icon-car', category: 'транспорт' },
-  { name: 'icon-couch', category: 'всё для дома' },
+  { name: 'icon-couch', category: 'все для дома' },
   { name: 'icon-tools', category: 'техника' },
-  {
-    name: 'icon-invoice',
-    category: 'коммуналка и связь',
-  },
-  { name: 'icon-clay', category: 'спорт и хобби' },
-  {
-    name: 'icon-book',
-    category: 'образование',
-  },
+  { name: 'icon-invoice', category: 'коммуналка, связь' },
+  { name: 'icon-clay', category: 'спорт, хобби' },
+  { name: 'icon-book', category: 'образование' },
   { name: 'icon-ufo', category: 'прочее' },
 ];
