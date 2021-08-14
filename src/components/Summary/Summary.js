630656fa65b539300d143712527caf508e34f52d
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2021-08-14 11:25:26 +0300

    переписал компонент Сводка

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,48 +1,26 @@
 import React, { useEffect } from 'react';
 import styles from './Summary.module.scss';
-import { useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import transactionsSelectors from '../../redux/selectors/transactionsSelectors';
 
-const Summary = () => {
-  const location = useLocation();
-  let transactions = useSelector(state =>
-    transactionsSelectors.getTransactions(state),
-  );
-  let data = {};
-  const options = { month: 'long' };
-  const monthToShow = [];
+const Summary = ({ summary }) => {
+  const currentMonthIndex = new Date().getMonth();
+  const summaryEntries = Object.entries(summary).reverse();
+  const neededMonths = [...summaryEntries].splice(11 - currentMonthIndex, 6);
 
-  let transactionsToShow = [];
+  const months = {
+    jan: 'январь',
+    feb: 'февраль',
+    mar: 'март',
+    apr: 'апрель',
+    may: 'май',
+    jun: 'июнь',
+    jul: 'июль',
+    aug: 'август',
+    sep: 'сентябрь',
+    oct: 'октябрь',
+    nov: 'ноябрь',
+    dec: 'декабрь',
+  };
 
-  if (location.pathname === '/income') {
-    transactionsToShow = transactions.incomes.incomes;
-  }
-  if (location.pathname === '/expense') {
-    transactionsToShow = transactions.expenses.expenses;
-  }
-  transactionsToShow?.forEach(tr => {
-    let date = new Date(tr.date);
-    let year = date.getFullYear();
-    let month = new Intl.DateTimeFormat('rus', options).format(date);
-    data = { ...data, [year]: { ...data[year], [month]: 0 } };
-  });
-  transactionsToShow?.forEach(tr => {
-    let date = new Date(tr.date);
-    let year = date.getFullYear();
-    let month = new Intl.DateTimeFormat('rus', options).format(date);
-    data[year][month] += tr.amount;
-  });
-
-  for (let i = 0; i < 6; i++) {
-    let currentDate = new Date();
-    currentDate.setMonth(currentDate.getMonth() - i);
-    const month = new Intl.DateTimeFormat('rus', options).format(currentDate);
-    const year = currentDate.getFullYear();
-    data[year]?.[month] !== undefined
-      ? monthToShow.push([month, data[year][month]])
-      : monthToShow.push([month, 0]);
-  }
   return (
     <table className={styles.table}>
       <tbody>
@@ -51,9 +29,9 @@ const Summary = () => {
             CВОДКА
           </th>
         </tr>
-        {monthToShow.map(item => (
+        {neededMonths.map(item => (
           <tr key={item[0]}>
-            <th className={styles.month}>{item[0]}</th>
+            <th className={styles.month}>{months[item[0]]}</th>
             <th className={styles.sum}>{item[1]}</th>
           </tr>
         ))}
